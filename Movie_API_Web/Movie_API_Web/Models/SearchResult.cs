3eade7562b5f3f70b64be03081db75dbb7f9c931
Zondag2k3@gmail.com <zondag2k3@gmail.com>
2026-02-21 01:19:37 +0200

    Added Search Result model

--- Movie_API_Web/Movie_API_Web/Models/SearchResult.cs
+++ Movie_API_Web/Movie_API_Web/Models/SearchResult.cs
@@ -2,5 +2,12 @@
 {
     public class SearchResult
     {
+        public int Page { get; set; }
+
+        public List<Movie> Results { get; set; } = new List<Movie>();
+
+        public int TotalResults { get; set; }
+
+        public int TotalPages { get; set; }
     }
 }
--- Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
+++ Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e476e52de3a9e647cba59b2d36d7419e181a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
