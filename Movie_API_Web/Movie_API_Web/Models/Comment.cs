fd87a01cd5be56a5e99e5b0b9caeeebd2dc54dec
Zondag2k3@gmail.com <zondag2k3@gmail.com>
2026-02-21 01:30:45 +0200

    Added Comment model

--- Movie_API_Web/Movie_API_Web/Models/Comment.cs
+++ Movie_API_Web/Movie_API_Web/Models/Comment.cs
@@ -2,5 +2,42 @@
 {
     public class Comment
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        
+        public int MovieId { get; set; }
+
+        
+        public string Author { get; set; }
+
+        
+        public string Email { get; set; }
+
+        
+        public string Message { get; set; }
+
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public List<Reply> Replies { get; set; } = new List<Reply>();
+    }
+
+    public class Reply
+    {
+        
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        
+        public string Author { get; set; }
+
+        
+        public string Email { get; set; }
+
+        
+        public string Message { get; set; }
+
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
