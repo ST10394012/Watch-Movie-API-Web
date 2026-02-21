157cb22f9201b17f8bcab6d928c35d1da0e00a2e
Zondag2k3@gmail.com <zondag2k3@gmail.com>
2026-02-21 14:03:12 +0200

    Updated the Homecontroller functionality

--- Movie_API_Web/Movie_API_Web/Controllers/HomeController.cs
+++ Movie_API_Web/Movie_API_Web/Controllers/HomeController.cs
@@ -1,20 +1,41 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Movie_API_Web.Models;
+using Movie_API_Web.Services;
 
 namespace Movie_API_Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly TmdbService _tmdbService;
+
+        public HomeController(TmdbService tmdbService)
         {
-            return View();
+            _tmdbService = tmdbService;
         }
 
-        public IActionResult Privacy()
+        
+        public async Task <IActionResult> Index()
         {
-            return View();
+            try
+            {
+                var popularMovies = await _tmdbService.GetPopularMoviesAsync();
+                ViewBag.FeatureMovies = popularMovies.Results.Take(6).ToList();
+
+                var upcomingMovies = await _tmdbService.GetUpcomingMoviesAsync();
+                ViewBag.UpcomingMovies = upcomingMovies.Results.Take(6).ToList();
+
+                return View();
+
+            }
+            catch (Exception ex) {
+                ViewBag.Error = $"Error loading movies: {ex.Message}";
+                return View();
+            }
+            
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
