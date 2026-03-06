210345260b5bd9c933e55bb01bfb3883ea0d68e9
Zondag2k3@gmail.com <166526935+SontagaMashalane@users.noreply.github.com>
2026-03-06 23:31:32 +0200

    Added home index view

--- Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
+++ Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54200f02189f4ce77a95cc267b8b9bfa666acdac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84440abd6a18067752e54779caadfde357b885d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
