913dd62dfce0860cbde7d5158d55e219477e8091
Zondag2k3@gmail.com <166526935+SontagaMashalane@users.noreply.github.com>
2026-03-07 00:04:41 +0200

    Added Details view

--- Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
+++ Movie_API_Web/Movie_API_Web/obj/Debug/net10.0/Movie_API_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e3b88970dfe5346a5b9e1a693589b6a3d46295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d034373436285e7e8cf7d33574ae2d1fbbbe35b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie_API_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
